60cffa20f205a389b624425d5558c681272e79d9
szytel <nate@nextreason.com>
2025-02-22 17:20:30 -0800

    Added generic client config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js
 const config = {
-    clientId: 'qecuhknq7vdtkx549vz2qakmhnebr99z', // Replace with your client ID
-    issuer: ' https://id.uat.eu.nextreason.com', // Replace with your issuer URL (e.g., https://your-next-identity-provider.com)
+    clientId: 'your-client-id', // Replace with your client ID
+    issuer: 'https://your-identity-provider.com', // Replace with your issuer URL 
     redirectUri: window.location.origin + '/callback', // Important: Add a callback route
     scopes: ['openid', 'profile', 'email'], // Add the scopes you need
 };
