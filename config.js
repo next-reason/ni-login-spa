a86da30c0af3718fedff1ec55a183c492a343a11
szytel <nate@nextreason.com>
2025-02-22 18:53:13 -0800

    js button

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 // config.js
+// const config = {
+//     clientId: 'your-client-id', // Replace with your client ID
+//     issuer: 'https://your-identity-provider.com', // Replace with your issuer URL (e.g., https://your-next-identity-provider.com)
+//     redirectUri: window.location.origin + '/callback', // Important: Add a callback route
+//     scopes: ['openid', 'profile', 'email'], // Add the scopes you need
+// };
 const config = {
-    clientId: 'your-client-id', // Replace with your client ID
-    issuer: 'https://your-identity-provider.com', // Replace with your issuer URL (e.g., https://your-next-identity-provider.com)
+    clientId: 'qecuhknq7vdtkx549vz2qakmhnebr99z', // Replace with your client ID
+    issuer: ' https://id.uat.eu.nextreason.com', // Replace with your issuer URL (e.g., https://your-next-identity-provider.com)
     redirectUri: window.location.origin + '/callback', // Important: Add a callback route
     scopes: ['openid', 'profile', 'email'], // Add the scopes you need
 };
-
 
